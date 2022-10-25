273452052623a3c19bb41aa7635a9fdf6bc1ff5a
xlsymphonylx <xlsymphonylx@gmail.com>
2022-10-24 21:38:57 -0600

    fixed post params

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import { Temp } from "../entity/Temp";
 import tempService from "../services/tempService";
 const router = Router();
 
-router.post("/temp", (req, res, next) => {
-  const { temp_corp, temp_amb } = req.body;
+router.post("/temp", (req: Request<Temp>, res, next) => {
+  const { temp_corp, temp_amb } = req.params;
   const newTemp = tempService.create({
     temp_amb: temp_amb,
     temp_corp: temp_corp,
