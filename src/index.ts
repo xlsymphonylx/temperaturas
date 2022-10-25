cac07c62bd5e0475bcdc881fc6a57d008dc1953a
xlsymphonylx <xlsymphonylx@gmail.com>
2022-10-24 21:31:28 -0600

    added env

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { AppDataSource } from "./data-source";
 import router from "./routes";
 import * as express from "express";
 import * as cors from "cors";
+import * as dotenv from "dotenv";
 
 AppDataSource.initialize()
   .then(async () => {
     const app = express();
-    const port = 8082;
+    dotenv.config();
+    const port = process.env.PORT;
 
     app.use(express.json());
     app.use(
@@ -19,6 +21,8 @@ AppDataSource.initialize()
     console.log(
       "Here you can setup and run express / fastify / any other framework."
     );
-    app.listen(port, () => console.log("listening port " + port));
+    app.listen(port || 5000, () =>
+      console.log("listening port " + port || 5000)
+    );
   })
   .catch((error) => console.log(error));
